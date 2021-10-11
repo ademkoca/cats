563c27b0337539e649b94dd3f1fd47fe973e25c3
Adem Koca <aademkocaa@gmail.com>
2021-10-11 18:21:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const searchBtn = document.getElementById('search-btn');
+const fact = document.getElementById('fact');
+
+
+// event listeners
+searchBtn.addEventListener('click', getFacts);
+
+function getFacts(){
+    // let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://catfact.ninja/fact?max_length=140`)
+    .then(response => response.json())
+    // .then(data => console.log(data));
+    .then(data => {
+        if(data){
+        let html = "";       
+            
+                html += `<p class="w3-large w3-center" id="fact-text">${data.fact}</p>`;                
+                fact.innerHTML = html;
+                // image.classList.remove('hidden');
+        }
+        else fact.innerHTML = `<p class="w3-large w3-center" id="fact-text">sorry</p>`;
+        
+    });
+}
