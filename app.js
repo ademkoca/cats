887cf01a464ad00f966aef933240058146ccd517
ademkoca <aademkocaa@gmail.com>
2024-10-28 13:38:19 +0100

    add comment

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-const searchBtn = document.getElementById('search-btn');
-const fact = document.getElementById('fact');
-
+const searchBtn = document.getElementById("search-btn");
+const fact = document.getElementById("fact");
 
 // event listeners
-searchBtn.addEventListener('click', getFacts);
-
-function getFacts(){
-    // let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://catfact.ninja/fact?max_length=80`)
-    .then(response => response.json())
+searchBtn.addEventListener("click", getFacts);
+// test
+function getFacts() {
+  // let searchInputTxt = document.getElementById('search-input').value.trim();
+  fetch(`https://catfact.ninja/fact?max_length=80`)
+    .then((response) => response.json())
     // .then(data => console.log(data));
-    .then(data => {
-        if(data){
-        let html = "";       
-            
-                html += `<p class="w3-large w3-center" id="fact-text">${data.fact}</p>`;                
-                fact.innerHTML = html;
-                // image.classList.remove('hidden');
-        }
-        else fact.innerHTML = `<p class="w3-large w3-center" id="fact-text">sorry</p>`;
-        
+    .then((data) => {
+      if (data) {
+        let html = "";
+
+        html += `<p class="w3-large w3-center" id="fact-text">${data.fact}</p>`;
+        fact.innerHTML = html;
+        // image.classList.remove('hidden');
+      } else
+        fact.innerHTML = `<p class="w3-large w3-center" id="fact-text">sorry</p>`;
     });
 }
